f917d7b7691e19f7ead597f1fd2caa5cec6f739e
Darshan S N <95928626+darshansn19@users.noreply.github.com>
2023-03-08 12:38:22 +0530

    Update ProductRepository.java

--- src/main/java/com/alvas/repository/ProductRepository.java
+++ src/main/java/com/alvas/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.alvas.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alvas.entity.Product;
@@ -9,4 +11,5 @@ import com.alvas.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	List<Product> findByProductIdIn(List<Long> productId);
+	Page<Product> findByProductNameContainingIgnoreCase(String productName, Pageable pageable);
 }
