2dc26b4d4a9c4d6f47281904c973e6a385165d1c
CHAITRA P SHETTY <64059838+Chaitrashetty2001@users.noreply.github.com>
2023-03-08 12:47:39 +0530

    Update PaymentRepository.java

--- src/main/java/com/alvas/repository/PaymentRepository.java
+++ src/main/java/com/alvas/repository/PaymentRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.alvas.entity.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+  Page<Payment> findByUserUserIdAndPaymentDateBetween(long userId, LocalDate monthStartDate, LocalDate monthEndDate,
+			Pageable pageable);
 
 }
