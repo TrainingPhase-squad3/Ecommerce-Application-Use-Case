a0d915c3b5948d1f6b7754f33c9aa4a68357edbb
Manoj Kumar M K <114489075+Manoj2208@users.noreply.github.com>
2023-03-08 12:38:08 +0530

    Update PaymentService.java

--- src/main/java/com/alvas/service/PaymentService.java
+++ src/main/java/com/alvas/service/PaymentService.java
@@ -1,5 +1,9 @@
 package com.alvas.service;
 
+import com.alvas.dto.PaymentDto;
+import com.alvas.response.ApiResponse;
+
 public interface PaymentService {
+	ApiResponse payment(long userId, PaymentDto paymentDto);
 
 }
