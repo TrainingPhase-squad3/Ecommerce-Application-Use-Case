9803bea12ca8ab9caef03fc20dca71f4fc0df548
Manoj Kumar M K <114489075+Manoj2208@users.noreply.github.com>
2023-03-08 12:39:46 +0530

    Update PaymentControllerTest.java

--- src/test/java/com/alvas/controller/PaymentControllerTest.java
+++ src/test/java/com/alvas/controller/PaymentControllerTest.java
@@ -1,5 +1,41 @@
 package com.alvas.controller;
 
-public class PaymentControllerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.alvas.dto.PaymentDto;
+import com.alvas.response.ApiResponse;
+import com.alvas.service.PaymentService;
+
+@ExtendWith(SpringExtension.class)
+class PaymentControllerTest {
+	
+	@Mock
+	PaymentService paymentService;
+	
+	@InjectMocks
+	PaymentController paymentController;
+	
+	@Test
+	void testPayment() {
+		long userId=1L;
+		PaymentDto paymentDto=new PaymentDto(1L, 1L);
+		ApiResponse apiResponse=new ApiResponse("purchase successfull",HttpStatus.CREATED);
+		Mockito.when(paymentService.payment(userId, paymentDto)).thenReturn(apiResponse);
+		ResponseEntity<ApiResponse> response=paymentController.payment(userId, paymentDto);
+		assertEquals(HttpStatus.CREATED,response.getStatusCode());
+		
+		
+		
+	}
+	
 
 }
