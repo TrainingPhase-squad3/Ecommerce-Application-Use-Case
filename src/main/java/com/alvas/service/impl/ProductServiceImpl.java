69157cb36453ef212b639a34ab31048b0d778ebe
Darshan S N <95928626+darshansn19@users.noreply.github.com>
2023-03-08 12:39:38 +0530

    Update ProductServiceImpl.java

--- src/main/java/com/alvas/service/impl/ProductServiceImpl.java
+++ src/main/java/com/alvas/service/impl/ProductServiceImpl.java
@@ -1,7 +1,36 @@
 package com.alvas.service.impl;
 
-import com.alvas.service.ProductService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import com.alvas.dto.ProductDto;
+import com.alvas.entity.Product;
+import com.alvas.exception.ProductNotFoundException;
+import com.alvas.repository.ProductRepository;
+import com.alvas.service.ProductService;
+@Service
 public class ProductServiceImpl implements ProductService {
+	@Autowired
+	ProductRepository productRepository;
+
+	@Override
+	public Page<ProductDto> getProduct(String productName, int pageNumber, int pageSize) {
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<Product> productPage = productRepository.findByProductNameContainingIgnoreCase(productName, pageable);
+		if (productPage.isEmpty()) {
+			throw new ProductNotFoundException("product is not available");
+		} else {
+			return productPage.map(p -> {
+				ProductDto productDto = new ProductDto();
+				BeanUtils.copyProperties(p, productDto);
+				return productDto;
+			});
+		}
+
+	}
 
 }
