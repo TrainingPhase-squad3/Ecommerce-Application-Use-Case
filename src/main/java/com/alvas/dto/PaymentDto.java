bc65536635a32180ceace04d307bf240f63de34a
Manoj Kumar M K <114489075+Manoj2208@users.noreply.github.com>
2023-03-08 12:33:45 +0530

    Add files via upload

---
+++ src/main/java/com/alvas/dto/PaymentDto.java
@@ -0,0 +1,18 @@
+package com.alvas.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDto {
+	@NotBlank
+	private long cartId;
+	@NotBlank
+	private long walletId;
+
+}
