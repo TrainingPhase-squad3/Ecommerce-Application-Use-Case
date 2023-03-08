4c1d5f0fb473ba184dedbfd7cc55ec02ebedfb51
CHAITRA P SHETTY <64059838+Chaitrashetty2001@users.noreply.github.com>
2023-03-08 12:48:30 +0530

    Update PaymentService.java

--- src/main/java/com/alvas/service/PaymentService.java
+++ src/main/java/com/alvas/service/PaymentService.java
@@ -5,5 +5,7 @@ import com.alvas.response.ApiResponse;
 
 public interface PaymentService {
 	ApiResponse payment(long userId, PaymentDto paymentDto);
+	List<PurchaseHistoryResponse> getUserPurchasesForMonth(Long userId, LocalDate monthStartDate, int pageNumber,
+			int pageSize);
 
 }
